50c2d17a75666df9572716a21a1bb8d1b33e09b2
Frank Thompson <frankthompson@gmail.com>
2014-01-21 23:08:15 -0600

    more stuff

--- Sudoku/sudoku.nim
+++ Sudoku/sudoku.nim
@@ -1,13 +1,10 @@
 # Sudoku solver
-# Based on Peter Norvig"s work
+# Based on Peter Norvig's Python solver
 # http://norvig.com/sudoku.html
 
 import tables
-
-# Cross product of elements in A and elements in B
-proc echoSeqStr(seqStr: seq[string]) =
-  for theStr in seqStr:
-    echo theStr
+import algorithm
+import strutils
 
 proc cross(A: string, B: string): seq[string] =
   result = @[]
@@ -24,10 +21,6 @@ proc charStrCross(A: char, B: string): seq[string] =
   result = @[]
   for j in B:
     result.add(A & j)
-
-proc getPuzzle(fname: string): string =
-  let puzzStr = readFile(fname)
-  return puzzStr
 
 proc `$` (inSeq: seq[string]): string =
   result = "["
@@ -52,7 +45,6 @@ let squares = cross(rows, cols)
 
 # Initialize unitlist
 var unitlist: seq[seq[string]] = @[]
-
 for c in cols:
   unitlist.add(strCharCross(rows, c))
 for r in rows:
@@ -71,17 +63,14 @@ for s in squares:
   units[s] = entry
 
 # Initialize peers
-# var peers = initTable[string, seq[string]]()
-# for s in squares:
-#   var entry: seq[string] = @[]
-#   for unit in units[s]:
-#     for peer in unit:
-#       echo peer
-#       echo unit
-#   peers[s] = entry
-
-echo unitlist
-echo units["C2"]
+var peers = initTable[string, seq[string]]()
+for s in squares:
+  var entry: seq[string] = @[]
+  for unit in units[s]:
+    for peer in unit:
+      if not (peer in entry) and peer != s:
+        entry.add(peer)
+  peers[s] = entry
 
 # A set of unit tests
 proc test() =
@@ -98,7 +87,47 @@ proc test() =
   assert units["C2"] == @[@["A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2", "I2"],
                           @["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9"],
                           @["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]]
+  assert peers["C2"] == @["A2", "B2", "D2", "E2", "F2", "G2", "H2", "I2", "C1", "C3",
+                         "C4", "C5", "C6", "C7", "C8", "C9", "A1", "A3", "B1", "B3"]
+  echo "All tests pass."
+
+proc load_grid(fname: string): string =
+  result = readFile(fname)
+
+proc grid_values(grid: string): TTable[string, char] =
+  result = initTable[string, char]()
+  var temp = squares
+  reverse(temp)
+  for c in grid:
+    case c
+    of '0','1','2','3','4','5','6','7','8','9':
+      result[pop(temp)] = c
+    of '.':
+      result[pop(temp)] = '0'
+    else: nil
+  assert len(result) == 81
+
+proc eliminate(values: var TTable[string, string], square: string, digit: char): bool =
+  if not digit in values[square]:
+    
+
+
+# proc assign(values: var TTable[string, string], square: string, digit: char): bool =
 
 
 
-test()
+# proc parse_grid(grid: string): TTable[string, string] =
+#   result = initTable[string, string]()
+#   for square, digit in grid_values(grid):
+#     if digit != '0':
+#       if not assign(values, square, digit):
+#         return nil
+
+proc test(tbl: var TTable[string, string]) =
+  echo "in test"
+  tbl["A2"] = ""
+  echo tbl["A2"]
+
+let grid_str = load_grid("simple.txt")
+#let pg = parse_grid(grid_str)
+#echo pg["A5"]
